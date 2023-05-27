040378cc1d7b5ed7060c61d139cc12968092692d
27Onion Nebell <57032603+onion108@users.noreply.github.com>
2023-05-27 20:40:20 +0800

    [feat] Added support for formatting strings.

--- src/util.rs
+++ src/util.rs
@@ -15,3 +15,79 @@ pub fn load_strings<P: AsRef<Path>>(path: P, name: &str) -> Result<Strings, std:
         Err(err) => Err(std::io::Error::new(std::io::ErrorKind::Other, err))
     }
 }
+
+/// Insert contents into the placeholders in the string.
+pub fn insert(fmt: &str, args: &[&str]) -> String {
+    let mut result = String::new();
+    let mut buffer = String::new();
+    let mut state = 0;
+
+    for i in fmt.chars() {
+        match state {
+            0 => {
+                match i {
+                    '{' => {
+                        state = 1;
+                    }
+                    _ => {
+                        result.push(i);
+                    }
+                }
+            }
+            1 => {
+                match i {
+                    '{' => {
+                        result.push('{');
+                        state = 0;
+                    }
+                    '0'..='9' => {
+                        state = 2;
+                        buffer.push(i);
+                    }
+                    _ => {
+                        state = 0;
+                    }
+                }
+            }
+            2 => {
+                match i {
+                    '0'..='9' => {
+                        buffer.push(i);
+                    }
+                    '}' => {
+                        result.push_str(args[str::parse::<usize>(&buffer).expect("Invalid number format")]);
+                        buffer = String::new();
+                        state = 0;
+                    }
+                    _ => {
+                        panic!("Invalid format string. ");
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    result
+}
+
+#[allow(unused)]
+mod test {
+    use super::insert;
+
+
+    #[test]
+    fn test_insert() {
+        let result = insert("The {2} fox jumps {0} the lazy {1}{20}", 
+        &[
+            "over", "dog", "brown", "", "", "",
+            "over", "dog", "brown", "", "", "",
+            "over", "dog", "brown", "", "", "",
+            "over", "dog", "?", "", "", "",
+            "?", "dog", "brown", "", "", "",
+        ]
+    );
+        assert_eq!(result, "The brown fox jumps over the lazy dog?");
+    }
+
+}
